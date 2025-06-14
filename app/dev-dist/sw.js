a4a38357c3c6d98f61932f8288547cae39663433
Purna Shrestha <crush9918@gmail.com>
2025-06-14 20:38:17 +0545

    feat: Implement logger and store on the file .log and .txt

--- app/dev-dist/sw.js
+++ app/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-6a4869df'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bj4gheol2ro"
+    "revision": "0.3af0nve9iao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
